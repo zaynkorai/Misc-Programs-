d2d88cab471fc8c5125eb2981610d16f9aadbe10
korai <zaynulabdin313@gmail.com>
2018-10-08 13:58:29 +0500

    files

---
+++ go/File/read.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+"fmt"
+"io/ioutil"
+)
+
+func main() {
+
+ bs, err := ioutil.ReadFile("test.txt")
+
+if err != nil{
+
+return }
+
+str :=  string(bs)
+
+fmt.Println(str)
+}
